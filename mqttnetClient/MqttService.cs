6fff64cc90c0edc9cd2dc936e5a2bf436590d70e
MarianoDesivo <marianodesivo96@gmail.com>
2022-11-28 09:56:57 -0300

    Add project files.

---
+++ mqttnetClient/MqttService.cs
@@ -0,0 +1,34 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+public class MqttService
+{
+    IMqttClient mqttClient;
+
+    public async Task Start(string brokerIp, string clientId, Action<string> callback = null)
+    {
+        var factory = new MqttFactory();
+
+        var options = new MqttClientOptionsBuilder()
+        .WithTcpServer(brokerIp)
+        .WithClientId(clientId)
+        .Build();
+
+        mqttClient = factory.CreateMqttClient();
+        await mqttClient.ConnectAsync(options, CancellationToken.None);
+        await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("MyTopic/test").Build());
+
+
+        mqttClient.PublishStringAsync("MyTopic/test");
+
+        //await mqttClient.ConnectAsync(options, CancellationToken.None);
+    }
+
+    
+    //public async Task SendCode(string message)
+    //{
+        
+    //    await mqttClient.PublishAsync("MyTopic/test", message);
+        
+    //}
+}
